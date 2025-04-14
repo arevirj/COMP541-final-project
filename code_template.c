27601eefd3692fa1f4eaf50bf4d2da619f75ff39
Justin Rivera <jrivera1@unc.edu>
2025-04-14 18:21:05 -0400

    still counting

--- code_template.c
+++ code_template.c
@@ -144,6 +144,39 @@ void initialize_IO(char* smem_initfile);
 //===============================================================
 
 
+void countdown(){
+	for(int i = 0; i < 100; i++){
+		//3
+		if(i==0){
+			putChar_atXY(1, 20, 14); putChar_atXY(1, 21, 14);
+									 putChar_atXY(1, 21, 15);
+			putChar_atXY(1, 20, 16); putChar_atXY(1, 21, 16);
+									 putChar_atXY(1, 21, 17);
+			putChar_atXY(1, 20, 18); putChar_atXY(1, 21, 18);
+		}
+		//2
+		else if (i == 33){
+			putChar_atXY(0, 21, 17);
+			putChar_atXY(1, 20, 17);
+		}
+		//1
+		else if (i == 67){
+			putChar_atXY(0, 20, 14);
+			putChar_atXY(0, 20, 16);
+			putChar_atXY(1, 21, 17);
+			putChar_atXY(0, 20, 17);
+			putChar_atXY(0, 20, 18);
+		}
+	}
+	//Ready for gameplay
+	putChar_atXY(0, 21, 14);
+	putChar_atXY(0, 21, 15);
+	putChar_atXY(0, 21, 16);
+	putChar_atXY(0, 21, 17);
+	putChar_atXY(0, 21, 18);
+}
+
+
 int main() {
 	initialize_IO("tron_board.mem");
 	
@@ -156,7 +189,6 @@ int main() {
 	int gamestart = 0;
 	
 	while(1){
-
 		//COUNTDOWN
 		if(gamestart == 0){
 		countdown();
@@ -209,38 +241,6 @@ int main() {
 		}
 	}	
 
-}
-
-void countdown(){
-	for(int i = 0; i < 100; i++){
-		//3
-		if(i==0){
-			putChar_atXY(1, 20, 14); putChar_atXY(1, 21, 14);
-									 putChar_atXY(1, 21, 15);
-			putChar_atXY(1, 20, 16); putChar_atXY(1, 21, 16);
-									 putChar_atXY(1, 21, 17);
-			putChar_atXY(1, 20, 18); putChar_atXY(1, 21, 18);
-		}
-		//2
-		else if (i == 33){
-			putChar_atXY(0, 21, 17);
-			putChar_atXY(1, 20, 17);
-		}
-		//1
-		else if (i == 67){
-			putChar_atXY(0, 20, 14);
-			putChar_atXY(0, 20, 16);
-			putChar_atXY(1, 21, 17);
-			putChar_atXY(0, 20, 17);
-			putChar_atXY(0, 20, 18);
-		}
-	}
-	//Ready for gameplay
-	putChar_atXY(0, 21, 14);
-	putChar_atXY(0, 21, 15);
-	putChar_atXY(0, 21, 16);
-	putChar_atXY(0, 21, 17);
-	putChar_atXY(0, 21, 18);
 }
 
 
