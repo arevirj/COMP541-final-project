8a8eba2f7c5faa7134b37ff5e35305e591dd7d25
Justin Rivera <jrivera1@unc.edu>
2025-04-14 20:54:22 -0400

    hello

--- code_template.c
+++ code_template.c
@@ -181,8 +181,7 @@ void countdown(){
 	putChar_atXY(0, 21, 16);
 }
 
-int wincheck(int p1, int p2){
-		put_sound(note_things[330825]);
+int wincheck(int p1, int p2){}
 		if(p1 & p2){
 			initialize_IO("tie.mem");
 			return(1);
